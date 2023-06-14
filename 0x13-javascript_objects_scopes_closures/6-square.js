42514647345b9b4388cdc3a7ce9346fb9f72c9f2
Amsalfitsum24 <amsalfessha21@gmail.com>
2023-06-14 13:25:48 -0700

    correctionoftask6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-
-const SquareModel = require('./5-square');
-
-module.exports = class Square extends SquareModel {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    this.print(c);
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
   }
 };
